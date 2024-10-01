8d4bcecef0cff3533fe7b076c20bcebd1619079f
MarcUsher <99440441+MarcUsher@users.noreply.github.com>
2024-10-01 09:16:35 +0100

    Update project & build to use relative links

--- config.rb
+++ config.rb
@@ -3,4 +3,7 @@ require_relative 'openapi_middleware'
 
 GovukTechDocs.configure(self)
 
+set :relative_links, true
+activate :relative_assets
+
 use OpenApiMiddleware
--- scripts/package.sh
+++ scripts/package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the site
-bundle exec middleman build --build-dir docs --verbose
+bundle exec middleman build --build-dir docs --relative-links --verbose
 
 # Create the artifact
 tar --dereference --directory docs -cvf artifact.tar --exclude=.git --exclude=.github .
