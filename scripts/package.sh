2be802a0157b2401bfac0dd538e98718d3cbcb4e
MarcUsher <99440441+MarcUsher@users.noreply.github.com>
2024-10-01 08:55:13 +0100

    Update workflow to use alternative artifact action
    - Script updated but commented out to bundle tar file in gzip format

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,3 +5,6 @@ bundle exec middleman build --build-dir docs --verbose
 
 # Create the artifact
 tar --dereference --directory docs -cvf artifact.tar --exclude=.git --exclude=.github .
+
+# # Create the gzip archive containing the tar file
+# gzip -c artifact.tar > github-pages.tar.gz
