d0121b37f5d7b233d8050a4f2f391d2e9ef35322
MarcUsher <99440441+MarcUsher@users.noreply.github.com>
2024-10-01 11:42:21 +0100

    Update config to copy openapi json to build dir

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
 require 'govuk_tech_docs'
 require_relative 'openapi_middleware'
+require 'fileutils'
+
 
 GovukTechDocs.configure(self)
 
@@ -10,3 +12,13 @@ activate :relative_assets
 base_path = ENV['BASE_PATH'] || ''
 
 use OpenApiMiddleware, base_path
+
+# Copy openapi.json to the build directory after the build process so that it can be exposed for use with SwaggerEditor online
+after_build do |builder|
+  src = File.join(root, './openapi/openapi.json')
+  dst = File.join(root, config[:build_dir], 'openapi', 'openapi.json')
+  puts "Copying #{src} to #{dst}"  # Debug statement
+  puts "BUILDIR #{config[:build_dir]}"
+  FileUtils.mkdir_p(File.dirname(dst))  # Ensure the destination directory exists
+  FileUtils.cp(src, dst)  # Copy the file
+end
